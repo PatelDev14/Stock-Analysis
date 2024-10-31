168668c3908f0723f42dd3952fa3fe49368732aa
PatelDev14 <devp1400@gmail.com>
2024-10-31 14:19:09 -0400

    Some minor changes to backend

--- server/server.js
+++ server/server.js
@@ -15,21 +15,6 @@ app.get('/', (req, res) => {
     res.send('Welcome to the Stock Analysis API!');
 });
 
-// Route for fetching stock quotes
-// app.get('/quote/:symbol', async (req, res) => {
-//     const symbol = req.params.symbol;
-//     console.log(`Fetching quote for symbol: ${symbol}`); // Log the requested symbol
-//     try {
-//         const quote = await yahooFinance.quote(symbol);
-//         console.log('Quote fetched:', quote); // Log the fetched quote
-//         res.json(quote);
-//     } catch (error) {
-//         console.error("Error fetching quote:", error); // Log the error
-//         res.status(500).json({ error: 'Failed to fetch quote' });
-//     }
-// });
-
-
 
 app.get('/quote/:symbol', async (req, res) => {
     const symbol = req.params.symbol;
@@ -94,18 +79,7 @@ app.get('/cash-flow/:symbol', async (req, res) => {
 });
 
 
-// Route for fetching financial statements
-// app.get('/financials/:symbol', async (req, res) => {
-//     const symbol = req.params.symbol;
-//     try {
-//         const balanceSheet = await yahooFinance.balanceSheet(symbol);
-//         const cashflowStatement = await yahooFinance.cashflowStatement(symbol);
-//         res.json({ balanceSheet, cashflowStatement });
-//     } catch (error) {
-//         console.error("Error fetching financial data:", error); // Log the error
-//         res.status(500).json({ error: 'Failed to fetch financial data' });
-//     }
-// });
+
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port} and CORS is enabled`);
